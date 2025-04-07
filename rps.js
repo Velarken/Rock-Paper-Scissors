4b69df0f45f4617c88b724987edac8c426a6e1c5
Dillon H. <181585418+Velarken@users.noreply.github.com>
2025-04-07 14:09:48 -0400

    Implemented buttons and event listeners

---
+++ rps.js
@@ -0,0 +1,49 @@
+
+/// Score variables
+let playerScore = 0;
+let computerScore = 0;
+                
+// Event listeners
+document.getElementById('rock').addEventListener('click',() => playGame('rock'));
+document.getElementById('paper').addEventListener('click',() => playGame('paper'));
+document.getElementById('scissors').addEventListener('click',() => playGame('scissors'));
+document.getElementById('playAgain').addEventListener('click',(playAgain));
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors']
+    let randomChoice = Math.floor(Math.random()*3);
+    return choices[randomChoice];
+};
+
+function playGame(playerChoice) {     
+    // Get computer choice
+    const computerChoice = getComputerChoice();
+
+    document.getElementById('player').textContent = `You chose:  ${playerChoice}`;
+    document.getElementById('npc').textContent = `Computer chose:  ${computerChoice}`;
+
+    const result = decideWinner(playerChoice, computerChoice);
+    document.getElementById('roundWinner').textContent = result;
+
+    // Update scores
+    document.getElementById('playerScore').textContent = playerScore;
+    document.getElementById('npcScore').textContent = computerScore;
+}
+
+function decideWinner(playerChoice,computerChoice) {
+    if (playerChoice === computerChoice) return "It's a tie!";
+
+    const winCondition = {
+        rock: "scissors",
+        paper: "rock",
+        scissors: "paper"
+    };
+
+    if (winCondition[playerChoice]=== computerChoice) {
+        playerScore ++
+        return "You win this round!";
+    } else {
+        computerScore++;
+        return "You were beaten this time!";
+    }
+} 
